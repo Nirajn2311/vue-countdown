50620201558b207cd3b2756dc0a7e420e0750c9a
Niraj Nandish <nirajnandish@icloud.com>
2019-06-12 19:21:54 +0400

    Created countdown

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,30 @@
   <div id="app">
     <p>Current Time - {{ time }}</p>
 	<datetime type='datetime' format='MMMM d yyyy, HH:mm:ss' value-zone='UTC+4' zone='UTC+4' v-model='datetime'></datetime>
-	<p>Chosen Time - {{ this.datetime }}</p>
+	<p>Chosen Time - {{ datetime }}</p>
+	<p>Remaining Time - {{ remtime }}</p>
   </div>
 </template>
 
 <script>
 import moment from 'moment'
+import countdown from 'countdown'
+require('moment-countdown')
 
 export default {
   name: 'app',
   data: function() {
     return {
 	time: '',
-	datetime: ''
+	datetime: '',
+	remtime: ''
     }
   },
   beforeMount() {
     setInterval(() => {
-      this.time = moment().format('MMMM D YYYY, HH:mm:ss')
+	  this.time = moment().format('MMMM D YYYY, HH:mm:ss')
+	  this.datetime = moment(this.datetime).format('MMMM D YYYY, HH:mm:ss')
+	  this.remtime = moment().countdown(this.datetime).toString()
     }, 1000)
   }
 }
